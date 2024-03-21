a177fe9dc37d8ed8eb3483aafe57bfb7017cd41a
Vinicius Hansel <hanelvinicius@gmail.com>
2024-03-21 20:11:37 -0300

    implemented upload route

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, send_file
+from flask import Flask, render_template, request, session
 
 import os
 import plotly
@@ -9,10 +9,13 @@ from plotly.offline import plot
 import pandas as pd
 import numpy as np
 
-from api.gnps import Proteosafe
-from api.utils import qiime2PCoA
+from gnps import Proteosafe
+from utils import qiime2PCoA,filterBlank
+from flask_dropzone import Dropzone
 
+dropzone = Dropzone()
 app = Flask(__name__)
+app.config['DROPZONE_DEFAULT_MESSAGE'] = 'Drop Down Your Archives Here'
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/graph', methods=['GET', 'POST'])
@@ -40,7 +43,27 @@ def downloadplot():
     pcoa_file = session.get('pcoa_file')
     return send_file(pcoa_file, as_attachment=True)
 
+@app.route("/upload",methods=['POST'])
+def upload():
+    file = request.files.get('file')
+    if not file:
+        return "Invalid Archive",400
+
+    if not file.content_type.endswith('csv'):
+        return 'Send Only CSV Files',400
+
+    try:
+        dataframe = pd.read_csv(file)
+        reformatedTable = filterBlank(dataframe)
+        
+    except Exception as e:
+        return  'Fatal Error Comunicate to the Administrator: ' +str(e), 500
+    
+    return 'SUCESSO',200
+
+
 if __name__=='__main__':
     #app.run(debug=True)
+    dropzone.init_app(app)
     app.run()
 
--- api/utils.py
+++ api/utils.py
@@ -8,6 +8,7 @@ from qiime2.plugins import metadata, feature_table, diversity, emperor
 from q2_emperor import plot, procrustes_plot, biplot, generic_plot
 from scipy.spatial.distance import squareform, pdist
 import skbio
+import uuid
 
 
 def qiime2PCoA(sample_metadata, df, out_dir, norm=True,
@@ -42,3 +43,44 @@ def qiime2PCoA(sample_metadata, df, out_dir, norm=True,
     else:
         emperor_plot.visualization.export_data(out_dir)
     return emperor_plot
+
+def filterBlank(feat_table, prop_blank_feats=1.0, prop_samples=0.9):
+    """Filters out features that have intensity less than a desired
+       proportion of intensity in blank samples at a given
+       number of samples
+    Parameters
+    ----------
+    feat_table: pd.DataFrame
+        DataFrame containing columns with ATTRIBUTE_ suffix.
+        Blank samples should contain "B(b)lank" in the name
+    prop_blank_feats: float
+        Proportion of blank intensitie that a feature has to be greater to be keept.
+    prop_samples: float
+        Proportion of samples in which the feature has to be greated than blank average proportion.
+    Returns
+        Filtered pd.DataFrame.
+    -------
+    """
+    last_attr = feat_table.columns[feat_table.columns.str.contains('ATTRIBUTE')][-1]
+    plast_attr = feat_table.columns.get_loc(last_attr)+1
+    mask = feat_table.filename.str.lower().str.contains('blank')
+    print('Found %s samples containing "blank" in the name.' % mask.sum())
+    print('Found %s features.' % (feat_table.shape[1]-plast_attr))
+    blank_mean = feat_table.loc[mask, feat_table.columns[plast_attr:]].mean()
+    feat_less_blank = (feat_table.loc[~mask, feat_table.columns[plast_attr:]]>(prop_blank_feats*blank_mean)).sum()
+    prop_feat_less_blank = feat_less_blank > prop_samples*(feat_table.shape[0]-mask.sum())
+    nprop = prop_feat_less_blank.sum()
+    print(f'''Found {nprop} features whose intensity was greater than {prop_blank_feats*100}% of the
+          average intensity of blank samples in {prop_samples*100}% samples at least.''')
+    return pd.concat([feat_table.loc[~mask, feat_table.columns[:plast_attr]],
+                      feat_table.loc[~mask, prop_feat_less_blank[prop_feat_less_blank].index]], axis=1)
+
+
+def createPcoa(meta,feat,metric):
+    taskid = uuid.uuid4()
+    if not os.path.exists(f'api/static/downloads/{taskid}'):
+            os.mkdir(f'api/static/downloads/{taskid}')
+    return qiime2PCoA(meta, feat,
+                              out_dir=f'api/static/downloads/{taskid}',
+                              metric=metric)
+     
