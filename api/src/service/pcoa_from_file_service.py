f067aca8122f961c861c15d6fbbd1fc8a4b80ef3
Vinicius Hansel <hanelvinicius@gmail.com>
2024-04-18 19:58:21 -0300

    refactor: service to create pcoaFromFile

---
+++ api/src/service/pcoa_from_file_service.py
@@ -0,0 +1,15 @@
+from api.PcoaFactory import PcoaFactory
+
+
+class PcoaFromFileService:
+
+    def __init__(self,session):
+        self.session = session
+
+    def _handleFile(self,file,fileId):
+        try:
+            factory = PcoaFactory(session=self.session)
+            factory.createPcoaFromFile(file,fileId)
+            return f'downloads/{fileId}/index.html'   
+        except Exception as e:
+            raise e 
