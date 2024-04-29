9a7cf80f78b08bb400f3b97ab4e6adfc6676bf54
Vinicius Hansel <hanelvinicius@gmail.com>
2024-04-29 18:48:13 -0300

    feat: get file from gnps implemented

--- api/src/controller/csv_from_gnps_controller.py
+++ api/src/controller/csv_from_gnps_controller.py
@@ -1,11 +1,38 @@
+import os
 from flask import Request
-
+from api.src.service.gnps import Proteosafe
+import pandas as pd
 
 class CsvFromGnpsController:
 
 
-    def __init__(self, request: Request, session):
-        pass
+    def __init__(self, request: Request, session,app):
+        self.request = request
+        self.session = session
+        self.app = app
     
-    def get_csv_from_gnps(request: Request):
-        pass
+    def get_csv_from_gnps(self):
+        taskId = self.request.form.get('taskId')
+        metric = self.request.form.get('metric')
+        self.app.logger.info(taskId)
+        self.app.logger.info(metric)
+
+
+        gnps = Proteosafe(taskId, 'FBMN')
+        gnps.get_gnps()
+        meta = gnps.meta
+        feat = gnps.feat
+        return self._createCsv(feat,meta)
+    
+    def _createCsv(self,feat,meta):
+        df2 = self._mergeDf(feat,meta)
+        directoryPath = os.path.join(os.getcwd(), 'api/static/downloads')
+        if not os.path.exists(directoryPath):
+            os.makedirs(directoryPath, exist_ok=True)
+        path = directoryPath+'/'+self.request.form.get('taskId')+ '.csv'
+        df2.to_csv(path)
+        return path
+        
+    def _mergeDf(self,feat,meta):
+        df = pd.concat([feat, meta], axis=1, join="inner")
+        return df 
