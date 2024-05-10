d0e9320f2ca1173b17aca471e1c0a29a0c6953ff
Vinicius Hansel <hanelvinicius@gmail.com>
2024-05-10 20:44:14 -0300

    refactor: dataProcessingConfig implement

--- api/src/controller/upload_edited_csv_controller.py
+++ api/src/controller/upload_edited_csv_controller.py
@@ -2,12 +2,14 @@ import os
 
 from flask import  redirect, url_for
 
+from api.src.model.DataProcessingConfig import DataProcessingConfig
+from api.src.service.pcoa_from_file_service import PcoaFromFileService
 from api.src.utils.utils import createFile, getFile
 
 
 class UploadEditedCsvController:
 
-    def __init__(self,request,session,app,pcoaFromFileService):
+    def __init__(self,request,session,app,pcoaFromFileService:PcoaFromFileService):
             self.request = request
             self.session = session
             self.app = app        
@@ -18,7 +20,7 @@ class UploadEditedCsvController:
         createFile(self.request,self.session,self.app)
         fullFilePath = getFile(self.session,self.app)
         fileId = self.session.get('fileId')
-
+        dataProcessingConfig = DataProcessingConfig(self.request.form['metric'], self.request.form['scaling'], self.request.form['normalization'], None)
         if fullFilePath is None:
             return "FileId not found in session", 400
             
@@ -28,5 +30,5 @@ class UploadEditedCsvController:
             return "File not found", 404    
         
         with open(file_path, 'rb') as file:
-            pcoa = self.pcoaFromFileService._handleFile(file,fileId)
+            pcoa = self.pcoaFromFileService._handleFile(file,fileId,dataProcessingConfig)
         return redirect(url_for('render_graph', pcoa=pcoa))
