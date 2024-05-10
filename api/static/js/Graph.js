7c3a26cfb8c159c364bbf8e751d34c13300a6bd2
Vinicius Hansel <hanelvinicius@gmail.com>
2024-05-10 11:37:43 -0300

    refactor: graph propper js file

---
+++ api/static/js/Graph.js
@@ -0,0 +1,54 @@
+function submitDropzone(event) {
+    const inputs = populeInputs();
+    
+    populeDragAndDropForm();
+    if(verifyForm(event,inputs) == false){
+        return;
+    }
+
+    document.getElementById("submit").hidden = true;
+}
+
+function populeInputs(){
+    const metric = document.getElementById('metric');
+    const normalization = document.getElementById('normalization');
+    const scaling = document.getElementById('scaling');
+
+    return [metric, normalization, scaling];
+}
+
+function populeDragAndDropForm(){
+    let sharedMetric = document.getElementById('shared-metric');
+    let sharedNormalization = document.getElementById('shared-normalization');
+    let sharedScaling = document.getElementById('shared-scaling');
+       
+    sharedMetric.value =  document.getElementById('metric').value;
+    sharedNormalization.value = document.getElementById('normalization').value;
+    sharedScaling.value = document.getElementById('scaling').value;
+
+}
+
+
+function verifyForm(event, inputs) {
+
+    for (let input of inputs) {
+
+        if (input.value === "") {
+            invalidInput(event,input);
+
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function invalidInput(event, input) {
+    event.preventDefault(); 
+    input.style.border = "1px solid red";
+
+    const optionElement = input.querySelector("option[value='']");
+    if (optionElement) {
+        optionElement.textContent = 'Please select a value';
+    }
+}
