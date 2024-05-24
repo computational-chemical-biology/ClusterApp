2dc81d1c4cfac9129f9b434c32a8ff109c8178c7
Vinicius Hansel <hanelvinicius@gmail.com>
2024-05-24 20:47:21 -0300

    fix: PQN normalization

--- api/src/service/factorys/normalization/NormalizationFactory.py
+++ api/src/service/factorys/normalization/NormalizationFactory.py
@@ -1,16 +1,16 @@
+from api.src.service.factorys.normalization.PqnNormalization import PqnNormalization
+
+
 class NormalizationFactory:
     
     def __init__(self,normType:str):
         self.normType = normType
 
-    #the idea here is to be an factory with different objects to represent different types of normalization
     def normalize(self,df):
 
         if self.normType == 'PQN':
-            medians = df.median(axis=1)
-            normalized_df = df.div(medians, axis=0)
-            normalized_df *= medians.median()
-            return normalized_df
+            pqn = PqnNormalization(df)
+            return pqn.normalize()
         
         elif self.normType == 'TIC':
             normalized_df = df.apply(lambda a: a/sum(a), axis=1)
