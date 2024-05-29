ead1b20c938d8a6f05546865fd104a5ffc1b3d36
Vinicius Hansel <hanelvinicius@gmail.com>
2024-05-29 11:36:37 -0300

    feat: pop up in file upload

--- api/static/js/Graph.js
+++ api/static/js/Graph.js
@@ -1,8 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const modalElement = document.getElementById('alert-modal');
+    removeModal(modalElement);
+});
+
+function removeModal(modalElement){
+    modalElement.addEventListener('hidden.bs.modal', function (event) {
+        document.body.classList.remove('modal-open');
+        const backdrop = document.querySelector('.modal-backdrop');
+        if (backdrop) {
+          backdrop.parentNode.removeChild(backdrop);
+        }
+    });
+}
+
 function submitDropzone(event) {
     const inputs = populeInputs();
     
     populeDragAndDropForm();
     if(verifyForm(event,inputs) == false){
+        showModal();
         return;
     }
 
@@ -46,4 +62,10 @@ function invalidInput(event, input) {
     if (optionElement) {
         optionElement.textContent = 'Please select a value';
     }
+}
+
+function showModal() {
+    const modalElement = document.getElementById('alert-modal');
+    const modal = new bootstrap.Modal(modalElement);
+    modal.show();
 }
