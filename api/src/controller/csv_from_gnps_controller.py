876c1e1b2942821ea723715ab24803f082f332fe
Vinicius Hansel <hanelvinicius@gmail.com>
2024-06-27 14:13:50 -0300

    fix: csvfromgnps feature

--- api/src/controller/csv_from_gnps_controller.py
+++ api/src/controller/csv_from_gnps_controller.py
@@ -1,4 +1,5 @@
 import os
+import re
 from flask import Request
 from api.src.service.gnps import Proteosafe
 import pandas as pd
@@ -29,6 +30,27 @@ class CsvFromGnpsController:
         return path
         
     def _mergeDf(self,feat,meta):
-        df = pd.concat([meta, feat], axis=1)
-        df.fillna('empty', inplace=True)
-        return df 
+        feat_df = self._rename_columns(feat)
+        df = pd.concat([meta, feat_df], axis=1)
+        return df
+
+    def _rename_columns(self,df):
+        new_columns = {}
+        for col in df.columns:
+            if col.strip() == '' or col.startswith('Unnamed:'):
+                df.drop(columns=[col], inplace=True)
+            elif col not in ['row ID', 'row m/z', 'row retention time']:
+                matches = re.findall(r'(\d+)', col)
+                if len(matches) >= 2:
+                    mz = matches[0]
+                    rt = matches[1]
+                    new_col_name = f"{mz}_{rt}"
+                    new_columns[col] = new_col_name
+                else:
+                    raise ValueError(f"Column name '{col}' does not contain enough numeric parts to form 'mz_rt' format.")
+        
+        df.rename(columns=new_columns, inplace=True)
+        
+        df.drop(columns=['row ID', 'row m/z', 'row retention time'], inplace=True)
+        
+        return df
