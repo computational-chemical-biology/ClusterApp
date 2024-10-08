69b1dc59cdfcb0f4531252885c5658dd3b2672c2
Vinicius Hansel <hanelvinicius@gmail.com>
2024-10-07 21:14:47 -0300

    fix: checkbox disable both filters

--- api/static/js/ToggleInputs.js
+++ api/static/js/ToggleInputs.js
@@ -1,8 +1,9 @@
-function toggleInputs(checkbox,propSample) {
+function toggleInputs(checkbox,propSample,feats) {
     const checkboxDom = document.getElementById(checkbox);
     const propSampleDom = document.getElementById(propSample);
-    
+    const featsDom = document.getElementById(feats);
     propSampleDom.value = '';
 
     propSampleDom.disabled = checkboxDom.checked;
+    featsDom.disabled = checkboxDom.checked;
 }
