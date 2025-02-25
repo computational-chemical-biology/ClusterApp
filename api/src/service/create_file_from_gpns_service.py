fbbadc8f63838108ee5adb1642d72fbbb2cc9319
Vinicius Hansel <hanelvinicius@gmail.com>
2025-02-24 21:41:27 -0300

    fix(create_file_from_gnps_service.py) merge dfs from gnps

--- api/src/service/create_file_from_gpns_service.py
+++ api/src/service/create_file_from_gpns_service.py
@@ -13,7 +13,7 @@ class CreateFileFromGnpsService:
         
     def _mergeDf(self,feat,meta):
         feat_df = self._rename_columns(feat)
-        df = pd.concat([meta, feat_df], axis=1)
+        df = self.merge_dataframes_dynamic(meta,feat_df)
         return df
 
     def _rename_columns(self,df):
@@ -29,12 +29,23 @@ class CreateFileFromGnpsService:
                     new_col_name = f"{mz}_{rt}"
                     new_columns[col] = new_col_name
                 else:
-                    raise ValueError(f"Column name '{col}' does not contain enough numeric parts to form 'mz_rt' format.")
+                    df.drop(columns=[col], inplace=True)
         
         df.rename(columns=new_columns, inplace=True)
         
         df.drop(columns=['row ID', 'row m/z', 'row retention time'], inplace=True)
         
         return df
+    
+    def merge_dataframes_dynamic(self,df1, df2):
+
+        n_rows = min(len(df1), len(df2))
+        
+        df1_trimmed = df1.iloc[:n_rows].reset_index(drop=True)
+        df2_trimmed = df2.iloc[:n_rows].reset_index(drop=True)
+        
+        merged_df = pd.concat([df1_trimmed, df2_trimmed], axis=1)
+        
+        return merged_df
 
     
